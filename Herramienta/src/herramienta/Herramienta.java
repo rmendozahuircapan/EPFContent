16d7fd21c56b5c09ef80c5c7b44eea99eef31a50
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2017-03-15 19:01:41 -0300

    Template, Artifact, Task
    Se rescata casi todo lo necesario de los elementos del proceso de
    software.

--- Herramienta/src/herramienta/Herramienta.java
+++ Herramienta/src/herramienta/Herramienta.java
@@ -6,11 +6,8 @@ public class Herramienta {
 
 
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        System.out.println("Hola");
-        String ruta_archivo = "C:\\Users\\Rodrigo\\Desktop\\Library\\Formalización Proceso\\plugin.xmi";
-        Contenido_XMI(ruta_archivo);
-        
+            String ruta_archivo = "C:\\Users\\Rodrigo\\Desktop\\Library\\Formalización Proceso\\plugin.xmi";
+            Contenido_XMI(ruta_archivo);
     }
     
     public static String linea_limpia(String linea){
@@ -39,62 +36,184 @@ public class Herramienta {
     }
     
     
-    public static void Rol(String linea){
+    public static void Mostrar_todo(String linea){// se buscan roles
         String[] aux = linea.split(" ");
         String[] limpio;
         if (aux[0].contains("contentElements")) {
             for (int i = 0; i < aux.length; i++) {
-                if (aux[i].contains("org.eclipse.epf.uma:Role")) {
-                    //System.out.println(linea);
-                    for (int j = 0; j < aux.length; j++) {
-                        if (aux[j].contains("name")) {
-                            limpio = aux[j].split("=");
-                            String name = limpio[1];
-                            limpio = name.split("\"");
-                            name = limpio[1];
-                            System.out.println("Name: "+ name);
-                        }
-                        if (aux[j].contains("presentationName")) {
-                            limpio = aux[j].split("=");
-                            String nombre = limpio[1];
-                            if (!nombre.endsWith("\"")) {
+                if (aux[i].contains("org.eclipse.epf.uma")){
+                    if(aux[i].contains("Role")){
+                        //System.out.println("Rol");
+                        for (int j = 0; j < aux.length; j++) {
+                            if (aux[j].contains("name")) {
+                                limpio = aux[j].split("=");
+                                String name = limpio[1];
+                                limpio = name.split("\"");
+                                name = limpio[1];
+                                //System.out.println("> Name: "+ name);
+                            }
+                            if (aux[j].contains("presentationName")) {
+                                limpio = aux[j].split("=");
+                                String nombre = limpio[1];
+                                if (!nombre.endsWith("\"")) {
+                                    boolean flag = true;
+                                    int k = j;
+                                    while (flag){
+                                        k++;
+                                        nombre = nombre + " " + aux[k];
+                                        if (aux[k].contains("\"")) {
+                                            flag = false;
+                                        }
+                                    }
+                                }
+                                limpio = nombre.split("\"");
+                                nombre = limpio[1];
+                                //System.out.println("Nombre: "+ nombre);
+                            }
+                            if (aux[j].contains("briefDescription")) {
+                                limpio = aux[j].split("=");
+                                String descripcion = limpio[1];
                                 boolean flag = true;
                                 int k = j;
                                 while (flag){
                                     k++;
-                                    nombre = nombre + " " + aux[k];
+                                    descripcion = descripcion + " " + aux[k];
                                     if (aux[k].contains("\"")) {
                                         flag = false;
                                     }
                                 }
+                                limpio = descripcion.split("\"");
+                                descripcion = limpio[1];
+                                //System.out.println("Descripción: "+ descripcion);
                             }
-                            limpio = nombre.split("\"");
-                            nombre = limpio[1];
-                            System.out.println("Nombre: "+ nombre);
                         }
-                        if (aux[j].contains("briefDescription")) {
-                            limpio = aux[j].split("=");
-                            String descripcion = limpio[1];
-                            boolean flag = true;
-                            int k = j;
-                            while (flag){
-                                k++;
-                                descripcion = descripcion + " " + aux[k];
-                                if (aux[k].contains("\"")) {
-                                    flag = false;
+                    }else if(aux[i].contains("Template")){
+                        //System.out.println(linea);
+                        //System.out.println("Template");
+                        for (int j = 0; j < aux.length; j++) {
+                            if (aux[j].contains("name")) {
+                                limpio = aux[j].split("=");
+                                String name = limpio[1];
+                                limpio = name.split("\"");
+                                name = limpio[1];
+                                //System.out.println("> Name: "+ name);
+                            }
+                            if (aux[j].contains("presentationName")) {
+                                limpio = aux[j].split("=");
+                                String nombre = limpio[1];
+                                if (!nombre.endsWith("\"")) {
+                                    boolean flag = true;
+                                    int k = j;
+                                    while (flag){
+                                        k++;
+                                        nombre = nombre + " " + aux[k];
+                                        if (aux[k].contains("\"")) {
+                                            flag = false;
+                                        }
+                                    }
                                 }
+                                limpio = nombre.split("\"");
+                                nombre = limpio[1];
+                                //System.out.println("Nombre: "+ nombre);
                             }
-                            limpio = descripcion.split("\"");
-                            descripcion = limpio[1];
-                            System.out.println("Descripción: "+ descripcion);
                         }
+                    }else if(aux[i].contains("Artifact")){ //Falta buscar e identificar cada uno de los templates asociados
+                        //System.out.println("Artifact");
+                        for (int j = 0; j < aux.length; j++) {
+                            if (aux[j].contains("name")) {
+                                limpio = aux[j].split("=");
+                                String name = limpio[1];
+                                limpio = name.split("\"");
+                                name = limpio[1];
+                                //System.out.println("> Name: "+ name);
+                            }
+                            if (aux[j].contains("presentationName")) {
+                                limpio = aux[j].split("=");
+                                String nombre = limpio[1];
+                                if (!nombre.endsWith("\"")) {
+                                    boolean flag = true;
+                                    int k = j;
+                                    while (flag){
+                                        k++;
+                                        nombre = nombre + " " + aux[k];
+                                        if (aux[k].contains("\"")) {
+                                            flag = false;
+                                        }
+                                    }
+                                }
+                                limpio = nombre.split("\"");
+                                nombre = limpio[1];
+                                //System.out.println("Nombre: "+ nombre);
+                            }
+                            if (aux[j].contains("briefDescription")) {
+                                limpio = aux[j].split("=");
+                                String descripcion = limpio[1];
+                                boolean flag = true;
+                                int k = j;
+                                while (flag){
+                                    k++;
+                                    descripcion = descripcion + " " + aux[k];
+                                    if (aux[k].contains("\"")) {
+                                        flag = false;
+                                    }
+                                }
+                                limpio = descripcion.split("\"");
+                                descripcion = limpio[1];
+                                //System.out.println("Descripción: "+ descripcion);
+                            }
+                        }
+                    }else if(aux[i].contains("Task")){ //Falta buscar e identificar cada uno de los templates asociados
+                        for (int j = 0; j < aux.length; j++) {
+                            if (aux[j].contains("name")) {
+                                limpio = aux[j].split("=");
+                                String name = limpio[1];
+                                limpio = name.split("\"");
+                                name = limpio[1];
+                                //System.out.println("> Name: "+ name);
+                            }
+                            if (aux[j].contains("presentationName")) {
+                                limpio = aux[j].split("=");
+                                String nombre = limpio[1];
+                                if (!nombre.endsWith("\"")) {
+                                    boolean flag = true;
+                                    int k = j;
+                                    while (flag){
+                                        k++;
+                                        nombre = nombre + " " + aux[k];
+                                        if (aux[k].contains("\"")) {
+                                            flag = false;
+                                        }
+                                    }
+                                }
+                                limpio = nombre.split("\"");
+                                nombre = limpio[1];
+                                //System.out.println("Nombre: "+ nombre);
+                            }
+                            if (aux[j].contains("briefDescription")) {
+                                limpio = aux[j].split("=");
+                                String descripcion = limpio[1];
+                                boolean flag = true;
+                                int k = j;
+                                while (flag){
+                                    k++;
+                                    descripcion = descripcion + " " + aux[k];
+                                    if (aux[k].contains("\"")) {
+                                        flag = false;
+                                    }
+                                }
+                                limpio = descripcion.split("\"");
+                                descripcion = limpio[1];
+                                //System.out.println("Descripción: "+ descripcion);
+                            }
+                        }
+                    }
+                    else{
+                        System.out.println(linea);
                     }
                 }
             }
             //System.out.println(linea);
         }
-    
-    
     }
     
     
@@ -114,12 +233,14 @@ public class Herramienta {
             String linea;
             while((linea=br.readLine())!=null){
                 //System.out.println(linea);
-                Rol(linea_limpia(linea));
+                //System.out.println(linea_limpia(linea));
+                Mostrar_todo(linea_limpia(linea));
                 
             }
         }
         catch(Exception e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.println("El archivo no se encuentra.");
         }finally{
             // En el finally cerramos el fichero, para asegurarnos
             // que se cierra tanto si todo va bien como si salta 
