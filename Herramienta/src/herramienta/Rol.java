093be861072aae0f057abf04679c8ea4548d6015
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2017-03-15 22:35:03 -0300

    Clase Rol
    Constructores, getter y setter

--- Herramienta/src/herramienta/Rol.java
+++ Herramienta/src/herramienta/Rol.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package herramienta;
 
-/**
- *
- * @author Rodrigo
- */
+
 public class Rol {
+    private String name;
+    private String nombre;
+    private String descripcion;
+    private String id;
+
     
+    public Rol(String name, String nombre, String descripcion, String id) {
+        this.name = name;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
