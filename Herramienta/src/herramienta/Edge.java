c492b1f137ad511ef0b7cd032a1e016c06a6f0a0
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2017-04-16 20:06:04 -0300

    Class Edge

---
+++ Herramienta/src/herramienta/Edge.java
@@ -0,0 +1,52 @@
+
+package herramienta;
+
+
+public class Edge {
+    private String id;
+    private String name;
+    private Node source;
+    private Node target;
+
+    public Edge(String id, String name, Node source, Node target) {
+        this.id = id;
+        this.name = name;
+        this.source = source;
+        this.target = target;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Node getSource() {
+        return source;
+    }
+
+    public void setSource(Node source) {
+        this.source = source;
+    }
+
+    public Node getTarget() {
+        return target;
+    }
+
+    public void setTarget(Node target) {
+        this.target = target;
+    }
+    
+    
+    
+}
