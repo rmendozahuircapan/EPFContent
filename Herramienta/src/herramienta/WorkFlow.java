3d53b8f3146293aac121849510733bccd66c4aee
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2017-04-16 21:01:53 -0300

    Class Workflow
    Cambio en los atributos

--- Herramienta/src/herramienta/WorkFlow.java
+++ Herramienta/src/herramienta/WorkFlow.java
@@ -4,16 +4,24 @@ package herramienta;
 import java.util.*;
 
 public class WorkFlow {
+    private String id;
     private String name;
-    private String type;
     private ArrayList<Node> nodes;
     private ArrayList<Edge> edges;
 
-    public WorkFlow(String name, String type, ArrayList<Node> nodes, ArrayList<Edge> edges) {
+    public WorkFlow(String id, String name, ArrayList<Node> nodes, ArrayList<Edge> edges) {
+        this.id = id;
         this.name = name;
-        this.type = type;
         this.nodes = nodes;
         this.edges = edges;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -22,14 +30,6 @@ public class WorkFlow {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public ArrayList<Node> getNodes() {
@@ -47,7 +47,6 @@ public class WorkFlow {
     public void setEdges(ArrayList<Edge> edges) {
         this.edges = edges;
     }
-    
-    
+
     
 }
