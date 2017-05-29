83f6cb54f413e682875033a466e6765e936a01eb
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2017-05-29 01:50:38 -0400

    Name plugin

--- EPFContent/src/EPFLibrary/Plugin.java
+++ EPFContent/src/EPFLibrary/Plugin.java
@@ -4,12 +4,18 @@ package EPFLibrary;
 import java.io.IOException;
 
 public class Plugin {
+    private String name;
     private MethodContent MethodContent;
     private Processes Processes;
 
     public Plugin(String path) throws IOException {
+        this.name = path.replace("\\", "%").split("%")[path.replace("\\", "%").split("%").length - 2];
         this.MethodContent = new MethodContent(path);
         this.Processes = new Processes(path);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public MethodContent getMethodContent() {
